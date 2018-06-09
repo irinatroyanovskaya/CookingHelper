b38e055c9fbc9f1fe5999c97902fc97a0c659d8e
irinatroyanovskaya <Ira_tro@mail.ru>
2018-06-09 15:08:17 +0300

    Class Library Added

---
+++ TelegramCookingHelper.Classes/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramCookingHelper.Classes
+{
+    class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Meal Meal { get; set; }
+        public MainIngredient MainIngredient { get; set; }
+    }
+}
